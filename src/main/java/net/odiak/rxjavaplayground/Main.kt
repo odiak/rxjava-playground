9986192113d41f6529fba161680df36275467e20
Kaido Iwamoto <h6824.kaido@gmail.com>
2016-09-21 17:02:47 +0900

    Try to handle RxJava's error

--- src/main/java/net/odiak/rxjavaplayground/Main.kt
+++ src/main/java/net/odiak/rxjavaplayground/Main.kt
@@ -2,9 +2,26 @@
 package net.odiak.rxjavaplayground
 
 import rx.Observable
+import rx.schedulers.Schedulers;
 
 fun main(args: Array<String>) {
-    Observable.just("oops!").subscribe {
-        println(it)
+    val obs = Observable.create<String> { o ->
+        o.onNext("A")
+        Thread.sleep(100)
+        o.onNext("B")
+        Thread.sleep(100)
+        o.onNext("C")
+        o.onError(Throwable("hello"))
+        o.onCompleted()
     }
+
+    obs
+            .doOnError({ e ->
+                println(e)
+            })
+            .subscribe({ s ->
+                println("onNext: $s")
+            }, { e ->
+                println("onError: $e")
+            })
 }
