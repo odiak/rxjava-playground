5213d8c6040f9c3ab17e87a57e2429a0e35c58ce
Kaido Iwamoto <h6824.kaido@gmail.com>
2016-09-21 15:58:56 +0900

    Setup RxJava

--- src/main/java/net/odiak/rxjavaplayground/Main.kt
+++ src/main/java/net/odiak/rxjavaplayground/Main.kt
@@ -1,6 +1,10 @@
 @file:JvmName("Main")
 package net.odiak.rxjavaplayground
 
+import rx.Observable
+
 fun main(args: Array<String>) {
-    println("hello")
+    Observable.just("oops!").subscribe {
+        println(it)
+    }
 }
