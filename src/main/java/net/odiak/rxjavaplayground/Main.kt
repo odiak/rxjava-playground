935b316b55da54794917d116189de60c117c5ce6
Kaido Iwamoto <h6824.kaido@gmail.com>
2016-10-14 13:22:57 +0900

    Use BehaviorSubject

--- src/main/java/net/odiak/rxjavaplayground/Main.kt
+++ src/main/java/net/odiak/rxjavaplayground/Main.kt
@@ -2,9 +2,24 @@
 package net.odiak.rxjavaplayground
 
 import rx.Observable
+import rx.subjects.BehaviorSubject
 
 fun main(args: Array<String>) {
-    Observable.just("oops!").subscribe {
-        println(it)
+    val sub = BehaviorSubject.create<String>()
+
+    sub.subscribe {
+        println("@1: $it")
     }
+
+    sub.onNext("A")
+    sub.onNext("B")
+    sub.onNext("C")
+
+    sub.subscribe {
+        println("@2: $it")
+    }
+
+    sub.onNext("D")
+    sub.onNext("E")
+    sub.onNext("F")
 }
